5913a51880fd30aa9d378efc0b40baeb0c3a3d6c
Idan Amati <idan.amati@gmail.com>
2021-01-15 09:59:13 +0200

    navbar user menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,52 @@
-import { Box, Container, Text, Circle, Flex, Button, Grid, IconButton } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Box, Container, Text, Flex, Button, Grid, IconButton } from '@chakra-ui/react';
 import { FaUser } from 'react-icons/fa';
+import Image from 'next/image';
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav>
       <Box shadow="lg" bg="white" width="full">
         <Container maxW="xl" centerContent p="3" justifyContent="space-between" flexDir="row">
           <Flex flexDir="row" alignItems="center">
-            <Circle bg="orange.300" w="8" h="8" mr="2" />
-            <Text fontSize="3xl">Batya</Text>
+            <Image
+              src="/woman_holding_a_balance_1942.9.97.jpg"
+              width="40"
+              height="40"
+              alt="Batya logo"
+            />
+            <Text ml="2" fontSize="4xl">
+              <h2>Batya</h2>
+            </Text>
           </Flex>
-          <Grid autoFlow="column" gap="4">
+          <Grid autoFlow="column" gap="4" position="relative">
             <Button variant="ghost">Buy</Button>
             <Button variant="ghost">Rent</Button>
-            <IconButton aria-label="menu" icon={<FaUser />} />
+            <IconButton
+              aria-label="menu"
+              icon={<FaUser />}
+              onClick={() => setOpen(!open)}
+              onBlur={() => setTimeout(() => setOpen(false), 100)}
+            />
+            {open && (
+              <Grid
+                p="4"
+                autoFlow="row"
+                gap="4"
+                position="absolute"
+                bg="white"
+                shadow="lg"
+                right="0"
+                rounded="lg"
+                transform="translateY(4.5rem)"
+                alignItems="start"
+              >
+                <Button variant="ghost">Login</Button>
+                <Button variant="ghost">Register</Button>
+              </Grid>
+            )}
           </Grid>
         </Container>
       </Box>
