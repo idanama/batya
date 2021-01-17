1d5451dcfe2a7cfe57a0abac9d91019ae5e26d88
Sagiy <sagi88@gmail.com>
2021-01-17 22:15:30 +0200

    created migration folder

---
+++ migrations/001-database.sql
@@ -0,0 +1,15 @@
+--UP
+
+CREATE TABLE User (
+   id INTEGER PRIMARY KEY AUTOINCREMENT,
+   firstName TEXT,
+   lastName TEXT,
+   email TEXT,
+   phone1 INTEGER,
+   password INTEGER
+)
+
+CREATE TABLE property(
+
+)
+--DOWN
