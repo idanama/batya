48a937e319e08899ff6ba9314845fd8340c6ef36
Sagiy <sagi88@gmail.com>
2021-01-28 00:31:20 +0200

    form submition to backend fixed bugs in add.jsx

--- pages/api/imgUpload.js
+++ pages/api/imgUpload.js
@@ -1,34 +1,7 @@
-// import nextConnect from 'next-connect';
-// import multer from 'multer';
-
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination: './public/uploads',
-//     filename: (req, file, cb) => cb(null, file.originalname),
-//   }),
-// });
-
-// const apiRoute = nextConnect({
-//   onError(error, req, res) {
-//     res.status(501).json({ error: `Sorry something Happened! ${error.message}` });
-//   },
-//   onNoMatch(req, res) {
-//     res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
-//   },
-// });
-
-// apiRoute.use(upload.array('theFiles'));
-
-// apiRoute.post((req, res) => {
-//   res.status(200).json({ data: 'success' });
-// });
-
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const multer = require('multer');
 
 cloudinary.uploader.upload_stream(function (error, result) {
-  console.log(result);
   return result;
 });
 
@@ -42,5 +15,3 @@ const storage = new CloudinaryStorage({
     },
   },
 });
-
-export const parser = multer({ storage: storage });
